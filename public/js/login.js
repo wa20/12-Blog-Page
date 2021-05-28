591c3f0b1150a68c3c8e9630f84e7944fd6e8819
wael <78425538+wa20@users.noreply.github.com>
2021-05-28 22:48:35 +0100

    new post code

--- public/js/login.js
+++ public/js/login.js
@@ -3,5 +3,5 @@ const logIn = document.getElementById("LogIn")
 logIn.addEventListener("click", async ()=>{
 
     const logInEmailVal = document.getElementById("logInEmail").value.trim()    
-
+    const logInEmailVal = document.getElementById("logInEmail").value.trim()   
 })
