e81ec45c443a2cf8876103e17b869c2eaed7e9cb
wael <78425538+wa20@users.noreply.github.com>
2021-05-30 23:17:36 +0100

    seed file code updates

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,2 +1,27 @@
 const sequelize = require('../config/connection');
-const {Comments, Post, CreatUser} = require('..')
+const {Comments, Post, CreatUser} = require('../models')
+
+const commentsData = require('./commentSeed');
+const postData = require('./postSeed') ;
+const userData = require('./userDataSeed');
+
+
+const SeedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    const user = await CreatUser.bulkCreate(userData,{
+        individualHooks: true,
+    })
+    //
+    for(const post of postData){
+        await Post.create({
+            ...post,
+            user_id: user[Math.floor(Math.random() * user.length)].id,
+        });
+    }
+
+    await Comments.bulkCreate(commentsData)
+    process.exit(0)
+
+
+}
